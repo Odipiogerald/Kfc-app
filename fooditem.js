e56932d73f75bf79a16dd065337b2f37afa34e33
Odipiogerald <136793186+Odipiogerald@users.noreply.github.com>
2023-12-01 15:36:11 +0300

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,394 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'CHEESE FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 13000,
+    img: 'images/F1.jpeg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'BOLOGNESE FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 15000,
+    img: 'images/F2.jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'LOADED FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 18000,
+    img: 'IMAGES/F4.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'CHILLY FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 15000,
+    img: 'IMAGES/fries.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'VEGETABLE FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/images (4).jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'MAYONASE FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 120000,
+    img: 'IMAGES/images (6).jpeg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'KETCHUP FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 13000,
+    img: 'IMAGES/images (7).jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'PLAIN FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 20000,
+    img: 'IMAGES/FR23 (1).jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'FRENCH FRIES',
+    category : 'FRIES',
+    rating : 4.3,
+    price: 15000,
+    img: 'IMAGES/FR23 (2).jpeg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'BUCKET DRUMSTICKS',
+    category : 'DRUMSTICKS',
+    rating : 4.3,
+    price: 11000,
+    img: 'IMAGES/drumsticks.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'CRISPY DRUMSTICKS',
+    category : 'DRUMSTCIKS',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/images (7).jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'EXTRA CRISPY DRUMSTICKS',
+    category : 'DRUMSTICK',
+    rating : 4.3,
+    price: 14000,
+    img: 'IMAGES/images (6).jpeg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'FRIED DRUMSTCIKS',
+    category : 'DRUMSTICKS',
+    rating : 4.3,
+    price: 12000,
+    img: 'IMAGES/D.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'AIR FRIED DRUMSTICKS',
+    category : 'DRUMSTICKS',
+    rating : 4.3,
+    price: 17000,
+    img: 'IMAGES/D2.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'SOUTHERN FRIED DRUMSTICKS',
+    category : 'DRUMSTICKS',
+    rating : 4.3,
+    price: 20000,
+    img: 'IMAGES/D3.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'FRIED RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 15000,
+    img: 'IMAGES/RICE 1.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'BIRYANI RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/R1.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'VEGETABLE RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 15000,
+    img: 'IMAGES/R2.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'CRISPY RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 12000,
+    img: 'IMAGES/RICE3.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'RIZO RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 8000,
+    img: 'IMAGES/RICE4.jpeg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'YAKINIKU RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 7000,
+    img: 'IMAGES/RICE2.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'COLONEL RICE',
+    category : 'RICE',
+    rating : 4.3,
+    price: 5000,
+    img: 'IMAGES/R3.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'PARATHA ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 8000,
+    img: 'IMAGES/ROLL1.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'SHAWARMA ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 7000,
+    img: 'IMAGES/ROLL2.jpeg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'FRIED ROLLS',
+    category : ' ROLLS',
+    rating : 4.3,
+    price: 5000,
+    img: 'IMAGES/ROLL3.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'ZINGERATHA ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 7000,
+    img: 'IMAGES/ROLL4.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'CRISPY ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/ROLL5.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'SLAB ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 4000,
+    img: 'IMAGES/ROLL6.jpeg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'VEGETABLE ROLLS',
+    category : 'ROLLS',
+    rating : 4.3,
+    price: 4000,
+    img: 'IMAGES/ROLL7.jpeg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'HAWAIIN KFCDOUBLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 7000,
+    img: 'IMAGES/DOUB2.jpeg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'KFCQUADRIPLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 5000,
+    img: 'IMAGES/DOUB3.jpeg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'MIGHTY KFCDOUBLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 16000,
+    img: 'IMAGES/images (25).jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'VEGETABLE KFCDOUBLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 14000,
+    img: 'IMAGES/images (26).jpeg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'ZINGER KFCDOUBLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/images (28).jpeg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'PHILLIPIANS KFCDOUBLE',
+    category : 'KFCDOUBLE',
+    rating : 4.3,
+    price: 8000,
+    img: 'IMAGES/images (27).jpeg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'ZINGER BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 18000,
+    img: 'IMAGES/BURG1.jpeg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'MINI BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 20000,
+    img: 'IMAGES/BURG2.jpeg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'MINI FILLET BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 12000,
+    img: 'IMAGES/BURG3.jpeg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'VEGETABLE BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 10000,
+    img: 'IMAGES/BURG7.jpeg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: ' CRUNCH BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 15000,
+    img: 'IMAGES/BURG5.jpeg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'BACON BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 40000,
+    img: 'IMAGES/BURG6.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'RED BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 25000,
+    img: 'IMAGES/BURG4.jpeg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'HAWAIIN BURGERS',
+    category : 'BURGERS',
+    rating : 4.3,
+    price: 34000,
+    img: 'IMAGES/BURG1.jpeg',
+    quantity: 1
+},
+]
+
+export {foodItem};
